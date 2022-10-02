352a67efe72b3d2802199ddc0176940c5798b8d2
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-10-02 14:37:31 +0100

    Added models

---
+++ Models/index.ts
@@ -0,0 +1,40 @@
+
+export enum Choices {
+    AllBalances = "View portfolio value for all tokens in your wallet",
+    OneBalance = "View portfolio value for any token in your wallet",
+    AllBalancesByDate = "View portfolio value for all tokens in your wallet on a particular date",
+    OneBalanceByDate = "View portfolio value for any token in your wallet on a particular date"
+}
+
+export interface IController {
+    handleGetATokenBalance(availableTokens: string[]): Promise<string>,
+    handleGetAllTokenBalances(): Promise<[]>,
+    handleGetAllTokenBalancesOnDate(): Promise<[]>,
+    handleGetATokenBalanceOnDate(availableTokens: string[]): Promise<string>
+}
+export interface Transaction {
+    timestamp: string;
+    transaction_type: TransactionType;
+    token: string;
+    amount: number
+}
+
+export interface TransactionStore {
+    name: string,
+    balance: number,
+    transactions: Transaction[]
+}
+
+export enum TransactionType {
+    withdrawal = "WITHDRAWAL",
+    deposit = "DEPOSIT"
+}
+
+
+export interface IRequestService {
+    getTransactions(token: string): Transaction[];
+    getTransactionsByDate(date: Date): TransactionStore;
+    getBalance(token: string);
+    getBalanceByDate(date: Date, token: string);
+    getAvailableTokens(): string[];
+}
